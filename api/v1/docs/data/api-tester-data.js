94c3c9ae168c81d272296be0f391ef9aa7c2089b
Paul Lynn <lynnporu@gmail.com>
2019-09-07 03:08:01 +0300

    Add errors

--- api/v1/docs/data/api-tester-data.js
+++ api/v1/docs/data/api-tester-data.js
@@ -81,5 +81,103 @@ APITester.classes = {
 }
 
 APITester.errors = {
-	/*errors will be here*/
+	ok: [
+		{
+			name: 'OK',
+			code: 200,
+			description: 'This error will be returned, if API script executed successfully. In this case body will contain JSON-info with error code and script result.',
+			body: {
+				errcode: 200,
+				result: {}
+			}
+		},
+		{
+			name: 'No Content',
+			code: 204,
+			description: 'This error will be returned, if API script executed successfully, but nothing to return.',
+			body: {
+				errcode: 204
+			}
+		}
+	],
+	client: [
+		{
+			name: 'Bad Request',
+			code: 400,
+			description: 'Some of GET parameters is absent.',
+			body: {
+				errcode: 400,
+				fields: [
+					'fieldexample1', 'fieldexample2'
+				]
+			}
+		},
+		{
+			name: 'Forbidden',
+			code: 403,
+			description: 'Your account are not allowed to perform this action.',
+			body: {
+				errcode: 403
+			}
+		},
+		{
+			name: 'Not Found',
+			code: 404,
+			description: 'No matches found.',
+			body: {
+				errcode: 404
+			}
+		},
+		{
+			name: 'Unprocessable Entity',
+			code: 422,
+			description: 'Some of fields you are passing is invalid.',
+			body: {
+				errcode: 422,
+				fields: [
+					{
+						name: 'email',
+						description: 'Too short. More than 10 symbols expected'
+					},
+					{
+						name: 'access_token',
+						description: 'Unsafe parameter. SHA-encrypted data expected.'
+					},
+					{
+						name: 'id',
+						description: 'String expected, but got Integer.'
+					}
+				]
+			}
+		}
+	],
+	server: [
+		{
+			name: 'Internal Server Error',
+			code: 500,
+			description: 'Script raised error. If debug-mode was turned on, body will also contain `debug` object.',
+			body: {
+				errcode: 500,
+				debug: {
+					time: '2018-12-18 12:13:24',
+					file: 'api/v1/script_name.php',
+					line: 43,
+					string: "Undefined variable $variable"
+				}
+			}
+		},
+		{
+			name: 'Bad Gateway',
+			code: 502,
+			description: 'This error will be returned, if script are trying to make requests to other server and they not respond or returns empty result. There will be also `debug` object, if debug-mode was turned on.',
+			body: {
+				errcode: 502,
+				debug: {
+					time: '18-12-2018 12:13',
+					url: '...',
+					response: '...'
+				}
+			}
+		}
+	]
 }
